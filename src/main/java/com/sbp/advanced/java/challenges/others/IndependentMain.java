ad14031f0fd08fb27985ec6dbe9c4ba221edda46
ArunDhwaj <sbp.arun.01@gmail.com>
2023-10-27 06:56:24 +0530

    Added Independed main

---
+++ src/main/java/com/sbp/advanced/java/challenges/others/IndependentMain.java
@@ -0,0 +1,11 @@
+package com.sbp.advanced.java.challenges.others;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class IndependentMain {
+    public static void main(String args[])
+    {
+      log.info("IndependentMain: Testing");
+    }
+}
